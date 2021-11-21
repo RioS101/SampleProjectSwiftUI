8ea3ae9a79442ced6a5f9a20a91e9f9977425026
RioS <rio.s101101@gmail.com>
2021-11-21 14:20:33 +0400

    Initial Commit

---
+++ SampleProjectSwiftUI/SampleProjectSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleProjectSwiftUIApp.swift
+//  SampleProjectSwiftUI
+//
+//  Created by RS on 21.11.21.
+//
+
+import SwiftUI
+
+@main
+struct SampleProjectSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
