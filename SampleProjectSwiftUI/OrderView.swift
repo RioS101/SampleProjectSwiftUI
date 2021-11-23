4326d2b93324d8db8a7d3fb6a03a188c3d028d82
RioS <rio.s101101@gmail.com>
2021-11-23 15:18:38 +0400

    Adding swipe to delete and EditButton

--- SampleProjectSwiftUI/OrderView.swift
+++ SampleProjectSwiftUI/OrderView.swift
@@ -21,6 +21,7 @@ struct OrderView: View {
                             Text("$\(item.price)")
                         }
                     }
+                    .onDelete(perform: deleteItems)
                 }
                 
                 Section {
@@ -28,10 +29,18 @@ struct OrderView: View {
                         Text("Place Order")
                     }
                 }
+                .disabled(order.items.isEmpty)
             }
             .navigationTitle("Order")
             .listStyle(InsetGroupedListStyle())
+            .toolbar(content: {
+                EditButton()
+            })
         }
+    }
+    
+    func deleteItems(at offsets: IndexSet) {
+        order.items.remove(atOffsets: offsets)
     }
 }
 
